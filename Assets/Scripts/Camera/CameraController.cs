c97f35695c9fafb83bcb1e8d652ef2ddcafc03a0
Javier Martín Escobar <javiermartinescobar.contact@gmail.com>
2023-12-24 11:21:48 +0100

    Add simple animations, movement and camera control

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 minPos;
+    [SerializeField]
+    private Vector2 maxPos;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float heightDiff;
+
+    public void Update() {
+        Vector3 currentPos = transform.position;
+        currentPos.x = Mathf.Lerp(currentPos.x, target.position.x, GlobalVariables.CAMERA_SMOTHNESS);
+        currentPos.y = Mathf.Lerp(currentPos.y, target.position.y + heightDiff, GlobalVariables.CAMERA_SMOTHNESS);
+
+        if(currentPos.x < minPos.x) currentPos.x = minPos.x;
+        if(currentPos.x > maxPos.x) currentPos.x = maxPos.x;
+        if(currentPos.y < minPos.y) currentPos.y = minPos.y;
+        if(currentPos.y > maxPos.y) currentPos.y = maxPos.y;
+        transform.position = currentPos;
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,24 +7,55 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+
+    [Header("Detectors")]
+    [SerializeField]
+    private Transform groundDetector;
+    [SerializeField]
+    private LayerMask groundMask;
 
     private Vector2 moveDir;
     private float xAxis;
+    private bool onGround;
+
     private Rigidbody2D rb;
+    private Animator anim;
 
     public void Start() {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     public void Update() {
         UpdateMovement();
+        UpdateAnimations();
     }
 
     private void UpdateMovement() {
         moveDir = rb.velocity;
         xAxis = Input.GetAxisRaw("Horizontal");
-        moveDir.y -= GlobalVariables.GRAVITY * Time.deltaTime;
+        if (xAxis != 0) {
+            transform.localScale = new Vector3(xAxis, 1, 1);
+        }
+
+        onGround = Physics2D.OverlapCircle(groundDetector.position, 0.1f, groundMask);
+        if(onGround && Input.GetKey(KeyCode.Space)) {
+            moveDir.y = jumpForce;
+            onGround = false;
+        }
+
+        if(!onGround) {
+            moveDir.y -= GlobalVariables.GRAVITY * Time.deltaTime;
+        }
         moveDir.x = xAxis * moveSpeed;
         rb.velocity = moveDir;
+    }
+
+    private void UpdateAnimations() {
+        anim.SetBool("moving", xAxis != 0);
+        anim.SetBool("onGround", onGround);
+        anim.SetFloat("yAxis", rb.velocity.y);
     }
 }
